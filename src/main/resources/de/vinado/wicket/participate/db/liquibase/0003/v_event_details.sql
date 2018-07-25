d644f04831479dfe46e6845e58b5c468da50663a
Vincent Nadoll <vincent.nadoll@gmail.com>
2018-07-25 19:03:56 +0200

    Add 8 more event detail views
    - MySQL databases cannot contain joins on subqueries in views

--- src/main/resources/de/vinado/wicket/participate/db/liquibase/0003/v_event_details.sql
+++ src/main/resources/de/vinado/wicket/participate/db/liquibase/0003/v_event_details.sql
@@ -1,3 +1,117 @@
+CREATE OR REPLACE VIEW v_event_member_accepted AS
+    SELECT
+        ev_member_int.event_id  AS event_id,
+        COUNT(ev_member_int.id) AS count_accepted
+    FROM m_member_event ev_member_int
+        INNER JOIN members member ON ev_member_int.member_id = member.id
+    WHERE ev_member_int.invitation_status = 0
+    GROUP BY ev_member_int.event_id;
+
+
+CREATE OR REPLACE VIEW v_event_member_declined AS
+    SELECT
+        ev_member_int.event_id       AS event_id,
+        COUNT(ev_member_int.id)      AS count_declined,
+        GROUP_CONCAT(person.first_name,
+                     ' ',
+                     person.last_name
+                     SEPARATOR ', ') AS declined_name
+    FROM m_member_event ev_member_int
+        INNER JOIN members member
+            ON ev_member_int.member_id = member.id
+        INNER JOIN persons person
+            ON member.person_id = person.id
+    WHERE ev_member_int.invitation_status = 1
+    GROUP BY ev_member_int.event_id;
+
+
+CREATE OR REPLACE VIEW v_event_member_pending AS
+    SELECT
+        ev_member_int.event_id  AS event_id,
+        COUNT(ev_member_int.id) AS count_pending
+    FROM m_member_event ev_member_int
+        INNER JOIN members member
+            ON ev_member_int.member_id = member.id
+    WHERE ev_member_int.invitation_status = 2
+        OR ev_member_int.invitation_status = 3
+    GROUP BY ev_member_int.event_id;
+
+
+CREATE OR REPLACE VIEW v_event_member_soprano AS
+    SELECT
+        ev_member_int.event_id       AS event_id,
+        COUNT(ev_member_int.id)      AS count_soprano,
+        GROUP_CONCAT(person.first_name,
+                     ' ',
+                     person.last_name
+                     SEPARATOR ', ') AS soprano_name
+    FROM m_member_event ev_member_int
+        INNER JOIN members member
+            ON ev_member_int.member_id = member.id
+        INNER JOIN persons person
+            ON member.person_id = person.id
+    WHERE
+        member.voice = 0 AND
+        ev_member_int.invitation_status = 0
+    GROUP BY ev_member_int.event_id;
+
+
+CREATE OR REPLACE VIEW v_event_member_alto AS
+    SELECT
+        ev_member_int.event_id       AS event_id,
+        COUNT(ev_member_int.id)      AS count_alto,
+        GROUP_CONCAT(person.first_name,
+                     ' ',
+                     person.last_name
+                     SEPARATOR ', ') AS alto_name
+    FROM m_member_event ev_member_int
+        INNER JOIN members member
+            ON ev_member_int.member_id = member.id
+        INNER JOIN persons person
+            ON member.person_id = person.id
+    WHERE
+        member.voice = 1 AND
+        ev_member_int.invitation_status = 0
+    GROUP BY ev_member_int.event_id;
+
+
+CREATE OR REPLACE VIEW v_event_member_tenor AS
+    SELECT
+        ev_member_int.event_id       AS event_id,
+        COUNT(ev_member_int.id)      AS count_tenor,
+        GROUP_CONCAT(person.first_name,
+                     ' ',
+                     person.last_name
+                     SEPARATOR ', ') AS tenor_name
+    FROM m_member_event ev_member_int
+        INNER JOIN members member
+            ON ev_member_int.member_id = member.id
+        INNER JOIN persons person
+            ON member.person_id = person.id
+        WHERE
+            member.voice = 2 AND
+            ev_member_int.invitation_status = 0
+    GROUP BY ev_member_int.event_id;
+
+
+CREATE OR REPLACE VIEW v_event_member_bass AS
+    SELECT
+        ev_member_int.event_id       AS event_id,
+        COUNT(ev_member_int.id)      AS count_bass,
+        GROUP_CONCAT(person.first_name,
+                     ' ',
+                     person.last_name
+                     SEPARATOR ', ') AS bass_name
+    FROM m_member_event ev_member_int
+        INNER JOIN members member
+            ON ev_member_int.member_id = member.id
+        INNER JOIN persons person
+            ON member.person_id = person.id
+    WHERE
+        member.voice = 3 AND
+        ev_member_int.invitation_status = 0
+    GROUP BY ev_member_int.event_id;
+
 CREATE OR REPLACE VIEW v_event_details AS
     SELECT
         ev.id                                      AS id,
@@ -31,108 +145,19 @@ CREATE OR REPLACE VIEW v_event_details AS
     FROM events ev
         LEFT JOIN m_member_event ev_member
             ON ev.id = ev_member.event_id
-        LEFT JOIN (SELECT
-                       ev_member_int.event_id  AS event_id,
-                       COUNT(ev_member_int.id) AS count_accepted
-                   FROM m_member_event ev_member_int
-                       INNER JOIN members member
-                           ON ev_member_int.member_id = member.id
-                   WHERE ev_member_int.invitation_status = 0
-                   GROUP BY ev_member_int.event_id) accepted
+        LEFT JOIN v_event_member_accepted accepted
             ON ev.id = accepted.event_id
-        LEFT JOIN (SELECT
-                       ev_member_int.event_id       AS event_id,
-                       COUNT(ev_member_int.id)      AS count_declined,
-                       GROUP_CONCAT(person.first_name,
-                                    ' ',
-                                    person.last_name
-                                    SEPARATOR ', ') AS declined_name
-                   FROM m_member_event ev_member_int
-                       INNER JOIN members member
-                           ON ev_member_int.member_id = member.id
-                       INNER JOIN persons person
-                           ON member.person_id = person.id
-                   WHERE ev_member_int.invitation_status = 1
-                   GROUP BY
-                       ev_member_int.event_id) declined
+        LEFT JOIN v_event_member_declined declined
             ON ev.id = declined.event_id
-        LEFT JOIN (SELECT
-                       ev_member_int.event_id  AS event_id,
-                       COUNT(ev_member_int.id) AS count_pending
-                   FROM m_member_event ev_member_int
-                       INNER JOIN members member
-                           ON ev_member_int.member_id = member.id
-                   WHERE ev_member_int.invitation_status = 2
-                         OR ev_member_int.invitation_status = 3
-                   GROUP BY ev_member_int.event_id) pending
+        LEFT JOIN v_event_member_pending pending
             ON ev.id = pending.event_id
-        LEFT JOIN (SELECT
-                       ev_member_int.event_id       AS event_id,
-                       COUNT(ev_member_int.id)      AS count_soprano,
-                       GROUP_CONCAT(person.first_name,
-                                    ' ',
-                                    person.last_name
-                                    SEPARATOR ', ') AS soprano_name
-                   FROM m_member_event ev_member_int
-                       INNER JOIN members member
-                           ON ev_member_int.member_id = member.id
-                       INNER JOIN persons person
-                           ON member.person_id = person.id
-                   WHERE
-                       member.voice = 0 AND
-                       ev_member_int.invitation_status = 0
-                   GROUP BY ev_member_int.event_id) soprano
+        LEFT JOIN v_event_member_soprano soprano
             ON ev.id = soprano.event_id
-        LEFT JOIN (SELECT
-                       ev_member_int.event_id       AS event_id,
-                       COUNT(ev_member_int.id)      AS count_alto,
-                       GROUP_CONCAT(person.first_name,
-                                    ' ',
-                                    person.last_name
-                                    SEPARATOR ', ') AS alto_name
-                   FROM m_member_event ev_member_int
-                       INNER JOIN members member
-                           ON ev_member_int.member_id = member.id
-                       INNER JOIN persons person
-                           ON member.person_id = person.id
-                   WHERE
-                       member.voice = 1 AND
-                       ev_member_int.invitation_status = 0
-                   GROUP BY ev_member_int.event_id) alto
+        LEFT JOIN v_event_member_alto alto
             ON ev.id = alto.event_id
-        LEFT JOIN (SELECT
-                       ev_member_int.event_id       AS event_id,
-                       COUNT(ev_member_int.id)      AS count_tenor,
-                       GROUP_CONCAT(person.first_name,
-                                    ' ',
-                                    person.last_name
-                                    SEPARATOR ', ') AS tenor_name
-                   FROM m_member_event ev_member_int
-                       INNER JOIN members member
-                           ON ev_member_int.member_id = member.id
-                       INNER JOIN persons person
-                           ON member.person_id = person.id
-                   WHERE
-                       member.voice = 2 AND
-                       ev_member_int.invitation_status = 0
-                   GROUP BY ev_member_int.event_id) tenor
+        LEFT JOIN v_event_member_tenor tenor
             ON ev.id = tenor.event_id
-        LEFT JOIN (SELECT
-                       ev_member_int.event_id       AS event_id,
-                       COUNT(ev_member_int.id)      AS count_bass,
-                       GROUP_CONCAT(person.first_name,
-                                    ' ',
-                                    person.last_name
-                                    SEPARATOR ', ') AS bass_name
-                   FROM m_member_event ev_member_int
-                       INNER JOIN members member
-                           ON ev_member_int.member_id = member.id
-                       INNER JOIN persons person
-                           ON member.person_id = person.id
-                   WHERE
-                       member.voice = 3 AND
-                       ev_member_int.invitation_status = 0
-                   GROUP BY ev_member_int.event_id) bass
+        LEFT JOIN v_event_member_bass bass
             ON ev.id = bass.event_id
         LEFT JOIN m_address_event ev_address
             ON ev.id = ev_address.event_id
