5ee90f73a35b6a9730757ff7490157f2bff6e18b
Vincent Nadoll <vincent.nadoll@gmail.com>
2018-07-28 19:13:21 +0200

    Remove tab panel on singers page

--- src/main/java/de/vinado/wicket/participate/ui/singer/SingerTabPanel.java
+++ src/main/java/de/vinado/wicket/participate/ui/singer/SingerTabPanel.java
@@ -1,6 +1,5 @@
 package de.vinado.wicket.participate.ui.singer;
 
-import de.agilecoders.wicket.core.markup.html.bootstrap.tabs.AjaxBootstrapTabbedPanel;
 import de.agilecoders.wicket.extensions.markup.html.bootstrap.icon.FontAwesomeIconType;
 import de.vinado.wicket.participate.component.modal.BootstrapModal;
 import de.vinado.wicket.participate.component.panel.BootstrapPanel;
@@ -14,8 +13,6 @@ import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.extensions.breadcrumb.IBreadCrumbModel;
 import org.apache.wicket.extensions.breadcrumb.panel.BreadCrumbPanel;
-import org.apache.wicket.extensions.markup.html.tabs.AbstractTab;
-import org.apache.wicket.extensions.markup.html.tabs.ITab;
 import org.apache.wicket.markup.html.link.AbstractLink;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.CompoundPropertyModel;
@@ -23,7 +20,6 @@ import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -38,37 +34,30 @@ public class SingerTabPanel extends BreadCrumbPanel {
     public SingerTabPanel(final String id, final IBreadCrumbModel breadCrumbModel) {
         super(id, breadCrumbModel);
 
-        final List<ITab> tabs = new ArrayList<>();
-        tabs.add(new AbstractTab(new ResourceModel("singers", "Singers")) {
+        final BootstrapPanel<List<Singer>> singersPanel = new BootstrapPanel<List<Singer>>("singersPanel",
+            new CompoundPropertyModel<>(personService.getSingers()), new ResourceModel("singers", "Singers")) {
             @Override
-            public Panel getPanel(final String panelId) {
-                return new BootstrapPanel<List<Singer>>(panelId, new CompoundPropertyModel<>(personService.getSingers()),
-                    new ResourceModel("singers", "Singers")) {
-                    @Override
-                    protected Panel newBodyPanel(final String id, final IModel<List<Singer>> model) {
-                        return new SingerListPanel(id, model);
-                    }
+            protected Panel newBodyPanel(final String id, final IModel<List<Singer>> model) {
+                return new SingerListPanel(id, model);
+            }
 
+            @Override
+            protected AbstractLink newDefaultBtn(final String id, final IModel<List<Singer>> model) {
+                setDefaultBtnLabelModel(new ResourceModel("singer.add", "Add Singer"));
+                setDefaultBtnIcon(FontAwesomeIconType.plus);
+                return new AjaxLink(id) {
                     @Override
-                    protected AbstractLink newDefaultBtn(final String id, final IModel<List<Singer>> model) {
-                        setDefaultBtnLabelModel(new ResourceModel("singer.add", "Add Singer"));
-                        setDefaultBtnIcon(FontAwesomeIconType.plus);
-                        return new AjaxLink(id) {
-                            @Override
-                            public void onClick(final AjaxRequestTarget target) {
-                                final BootstrapModal modal = ((BasePage) getWebPage()).getModal();
-                                modal.setContent(new AddEditSingerPanel(modal, new ResourceModel("singer.add", "Add Singer"),
-                                    new CompoundPropertyModel<>(new SingerDTO())));
-                                modal.show(target);
-                            }
-                        };
+                    public void onClick(final AjaxRequestTarget target) {
+                        final BootstrapModal modal = ((BasePage) getWebPage()).getModal();
+                        modal.setContent(new AddEditSingerPanel(modal, new ResourceModel("singer.add", "Add Singer"),
+                            new CompoundPropertyModel<>(new SingerDTO())));
+                        modal.show(target);
                     }
                 };
             }
-        });
+        };
 
-        final AjaxBootstrapTabbedPanel<ITab> tabbedPanel = new AjaxBootstrapTabbedPanel<>("tabbedPanel", tabs);
-        add(tabbedPanel);
+        add(singersPanel);
     }
 
     @Override
