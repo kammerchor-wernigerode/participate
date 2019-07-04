ef13d7459af798344bb4b1747a40c01f7374934c
Vincent Nadoll <me@vinado.de>
2019-07-04 21:55:48 +0200

    Add optional features

---
+++ src/main/java/de/vinado/wicket/participate/configuration/Feature.java
@@ -0,0 +1,16 @@
+package de.vinado.wicket.participate.configuration;
+
+/**
+ * Proprietary but optional application features.
+ *
+ * @author Vincent Nadoll
+ */
+public enum Feature {
+
+    /**
+     * If enabled, pending invitation will receive a reminder notification.
+     *
+     * @see de.vinado.wicket.participate.tasks.InvitationTask
+     */
+    REMIND_OVERDUE,
+}
