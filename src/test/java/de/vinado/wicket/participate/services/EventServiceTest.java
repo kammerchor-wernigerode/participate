2f91c20e49d50d83e0904c1e176ccc6cc95120a7
Vincent Nadoll <me@vinado.de>
2019-10-29 21:26:54 +0100

    Remove Lombok var annotation

--- src/test/java/de/vinado/wicket/participate/services/EventServiceTest.java
+++ src/test/java/de/vinado/wicket/participate/services/EventServiceTest.java
@@ -2,8 +2,8 @@ package de.vinado.wicket.participate.services;
 
 import de.vinado.wicket.participate.configuration.ApplicationProperties;
 import de.vinado.wicket.participate.email.service.EmailService;
+import de.vinado.wicket.participate.model.Event;
 import de.vinado.wicket.participate.model.Participant;
-import lombok.var;
 import org.apache.commons.lang3.time.DateUtils;
 import org.junit.Before;
 import org.junit.Test;
@@ -28,9 +28,9 @@ public class EventServiceTest {
 
     @Before
     public void setUp() {
-        var personService = mock(PersonService.class);
-        var emailService = mock(EmailService.class);
-        var applicationProperties = mock(ApplicationProperties.class);
+        PersonService personService = mock(PersonService.class);
+        EmailService emailService = mock(EmailService.class);
+        ApplicationProperties applicationProperties = mock(ApplicationProperties.class);
 
         service = new EventServiceImpl(personService, emailService, applicationProperties);
 
@@ -39,22 +39,22 @@ public class EventServiceTest {
 
     @Test
     public void testDeadlineAfter() {
-        var event = mockEvent(1L, DateUtils.addDays(NOW, 7));
-        var participant = mock(Participant.class);
+        Event event = mockEvent(1L, DateUtils.addDays(NOW, 7));
+        Participant participant = mock(Participant.class);
         when(participant.getEvent()).thenReturn(event);
 
-        var after = service.hasDeadlineExpired(participant);
+        boolean after = service.hasDeadlineExpired(participant);
 
         assertTrue(after);
     }
 
     @Test
     public void testDeadlineBefore() {
-        var event = mockEvent(1L, DateUtils.addDays(NOW, 30));
-        var participant = mock(Participant.class);
+        Event event = mockEvent(1L, DateUtils.addDays(NOW, 30));
+        Participant participant = mock(Participant.class);
         when(participant.getEvent()).thenReturn(event);
 
-        var after = service.hasDeadlineExpired(participant);
+        boolean after = service.hasDeadlineExpired(participant);
 
         assertFalse(after);
     }
