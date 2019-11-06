e770ac24a6c1591a7ddab36a79517abe6e66bd46
Vincent Nadoll <me@vinado.de>
2019-11-06 17:58:06 +0100

    Introduce person entity related repositories

---
+++ src/main/java/de/vinado/wicket/participate/services/SingerRepository.java
@@ -0,0 +1,11 @@
+package de.vinado.wicket.participate.services;
+
+import de.vinado.wicket.participate.model.Singer;
+
+/**
+ * Repository for managing singers.
+ *
+ * @author Vincent Nadoll
+ */
+public interface SingerRepository extends PersonBaseRepository<Singer> {
+}
