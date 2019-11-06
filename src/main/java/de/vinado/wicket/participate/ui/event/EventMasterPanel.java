284b67084972a5a8f425b557996d1891ebc603f0
Vincent Nadoll <me@vinado.de>
2019-11-06 16:36:14 +0100

    Remove unused imports

--- src/main/java/de/vinado/wicket/participate/ui/event/EventMasterPanel.java
+++ src/main/java/de/vinado/wicket/participate/ui/event/EventMasterPanel.java
@@ -47,8 +47,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import static com.pivovarit.function.ThrowingFunction.sneaky;
