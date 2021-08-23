6c4a6f54855926e6a84a21a525eaf9434d909ee6
Vincent Nadoll <vincent.nadoll@googlemail.com>
2021-08-23 20:46:01 +0200

    Remove unecessary jQuery dependency

--- src/main/java/de/vinado/wicket/participate/resources/js/RelativeTimePipeJsResourceReference.java
+++ src/main/java/de/vinado/wicket/participate/resources/js/RelativeTimePipeJsResourceReference.java
@@ -1,11 +1,6 @@
 package de.vinado.wicket.participate.resources.js;
 
-import org.apache.wicket.Application;
-import org.apache.wicket.markup.head.HeaderItem;
-import org.apache.wicket.markup.head.JavaScriptHeaderItem;
 import org.apache.wicket.request.resource.JavaScriptResourceReference;
-
-import java.util.List;
 
 /**
  * @author Vincent Nadoll (vincent.nadoll@gmail.com)
@@ -16,13 +11,5 @@ public class RelativeTimePipeJsResourceReference extends JavaScriptResourceRefer
 
     public RelativeTimePipeJsResourceReference() {
         super(RelativeTimePipeJsResourceReference.class, "relative-time.pipe.js");
-    }
-
-    @Override
-    public List<HeaderItem> getDependencies() {
-        final List<HeaderItem> dependencies = super.getDependencies();
-        dependencies.add(JavaScriptHeaderItem.forReference(Application.get().getJavaScriptLibrarySettings().getJQueryReference()));
-
-        return dependencies;
     }
 }
