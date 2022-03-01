2cd5ed73f06d760594a6cfa3890f72b075de6a00
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-03-01 22:30:28 +0100

    Reorder imports

--- src/main/java/de/vinado/wicket/participate/model/Participant.java
+++ src/main/java/de/vinado/wicket/participate/model/Participant.java
@@ -7,6 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Enumerated;
@@ -18,7 +19,6 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
-import java.util.Date;
 
 import static de.vinado.wicket.participate.model.InvitationStatus.ACCEPTED;
 import static de.vinado.wicket.participate.model.InvitationStatus.DECLINED;
