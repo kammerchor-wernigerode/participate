0f4085cc69db69b6ae972ae8b6a18ed69c829116
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-03-07 21:40:16 +0100

    Focus username text field

--- src/main/java/de/vinado/wicket/participate/ui/login/SignInPanel.java
+++ src/main/java/de/vinado/wicket/participate/ui/login/SignInPanel.java
@@ -2,6 +2,7 @@ package de.vinado.wicket.participate.ui.login;
 
 import de.agilecoders.wicket.core.markup.html.bootstrap.behavior.CssClassNameAppender;
 import de.agilecoders.wicket.core.markup.html.bootstrap.common.NotificationPanel;
+import de.vinado.wicket.participate.behavoirs.FocusBehavior;
 import org.apache.wicket.Component;
 import org.apache.wicket.feedback.FeedbackMessage;
 
@@ -21,6 +22,7 @@ public class SignInPanel extends org.apache.wicket.authroles.authentication.pane
         super.onInitialize();
 
         addOrReplace(alertPanel("feedback"));
+        getForm().get("username").add(new FocusBehavior());
     }
 
     private Component alertPanel(String id) {
