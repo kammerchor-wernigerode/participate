ae718cf3a806b3268252af860214e19f353a03c2
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-03-21 14:29:12 +0100

    Derive Bootstrap optimized authenticated web app

---
+++ src/main/java/de/vinado/wicket/bt4/AuthenticatedBootstrapWebApplication.java
@@ -0,0 +1,169 @@
+package de.vinado.wicket.bt4;
+
+import com.google.javascript.jscomp.CompilationLevel;
+import de.agilecoders.wicket.core.Bootstrap;
+import de.agilecoders.wicket.core.markup.html.RenderJavaScriptToFooterHeaderResponseDecorator;
+import de.agilecoders.wicket.core.request.resource.caching.version.Adler32ResourceVersion;
+import de.agilecoders.wicket.core.settings.BootstrapSettings;
+import de.agilecoders.wicket.core.settings.IBootstrapSettings;
+import de.agilecoders.wicket.extensions.javascript.GoogleClosureJavaScriptCompressor;
+import de.agilecoders.wicket.extensions.javascript.YuiCssCompressor;
+import de.vinado.wicket.participate.ui.pages.ErrorPage;
+import de.vinado.wicket.participate.ui.pages.ExpiredPage;
+import org.apache.wicket.RuntimeConfigurationType;
+import org.apache.wicket.authroles.authentication.AuthenticatedWebApplication;
+import org.apache.wicket.authroles.authentication.AuthenticatedWebSession;
+import org.apache.wicket.authroles.authorization.strategies.role.RoleAuthorizationStrategy;
+import org.apache.wicket.core.request.handler.PageProvider;
+import org.apache.wicket.core.request.handler.RenderPageRequestHandler;
+import org.apache.wicket.csp.ContentSecurityPolicySettings;
+import org.apache.wicket.request.IRequestHandler;
+import org.apache.wicket.request.cycle.IRequestCycleListener;
+import org.apache.wicket.request.cycle.RequestCycle;
+import org.apache.wicket.request.resource.caching.FilenameWithVersionResourceCachingStrategy;
+import org.apache.wicket.request.resource.caching.NoOpResourceCachingStrategy;
+import org.apache.wicket.request.resource.caching.version.CachingResourceVersion;
+import org.apache.wicket.serialize.java.DeflatedJavaSerializer;
+import org.apache.wicket.settings.ApplicationSettings;
+import org.apache.wicket.settings.DebugSettings;
+import org.apache.wicket.settings.MarkupSettings;
+import org.apache.wicket.settings.RequestCycleSettings;
+import org.apache.wicket.settings.ResourceSettings;
+import org.apache.wicket.settings.SecuritySettings;
+import org.springframework.util.StringUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+
+import static org.apache.wicket.RuntimeConfigurationType.DEPLOYMENT;
+import static org.apache.wicket.RuntimeConfigurationType.DEVELOPMENT;
+
+/**
+ * @author Vincent Nadoll
+ */
+public abstract class AuthenticatedBootstrapWebApplication extends AuthenticatedWebApplication {
+
+    @Override
+    protected void internalInit() {
+        super.internalInit();
+
+        ApplicationSettings applicationSettings = getApplicationSettings();
+        configureApplication(applicationSettings);
+    }
+
+    protected void configureApplication(ApplicationSettings applicationSettings) {
+        applicationSettings.setPageExpiredErrorPage(ExpiredPage.class);
+        applicationSettings.setInternalErrorPage(ErrorPage.class);
+        applicationSettings.setUploadProgressUpdatesEnabled(true);
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+
+        ContentSecurityPolicySettings cspSettings = getCspSettings();
+        configureCps(cspSettings);
+
+        MarkupSettings markupSettings = getMarkupSettings();
+        configureMarkup(markupSettings);
+
+        SecuritySettings securitySettings = getSecuritySettings();
+        configureSecurity(securitySettings);
+
+        ResourceSettings resourceSettings = getResourceSettings();
+        configureResourceManagement(resourceSettings);
+
+        mountPages();
+
+        installBootstrap();
+        optimize(resourceSettings);
+
+        DebugSettings debugSettings = getDebugSettings();
+        configureDebug(debugSettings);
+
+        installSpringComponentScanning();
+
+        getRequestCycleListeners().add(new IRequestCycleListener() {
+            @Override
+            public IRequestHandler onException(final RequestCycle cycle, final Exception ex) {
+                return new RenderPageRequestHandler(new PageProvider(new ErrorPage(ex)));
+            }
+        });
+    }
+
+    protected void configureCps(ContentSecurityPolicySettings cspSettings) {
+        cspSettings.blocking().disabled();
+    }
+
+    protected void configureMarkup(MarkupSettings markupSettings) {
+        markupSettings.setDefaultMarkupEncoding(StandardCharsets.UTF_8.name());
+        if (matches(DEPLOYMENT)) {
+            markupSettings.setCompressWhitespace(true);
+            markupSettings.setStripComments(true);
+        }
+    }
+
+    protected void configureSecurity(SecuritySettings securitySettings) {
+        RoleAuthorizationStrategy strategy = new RoleAuthorizationStrategy(roles ->
+            AuthenticatedWebSession.get().getRoles().hasAnyRole(roles));
+        securitySettings.setAuthorizationStrategy(strategy);
+    }
+
+    protected void configureResourceManagement(ResourceSettings resourceSettings) {
+        if (matches(DEPLOYMENT)) resourceSettings.setResourcePollFrequency(null);
+    }
+
+    protected abstract void mountPages();
+
+    protected void installBootstrap() {
+        IBootstrapSettings settings = new BootstrapSettings();
+        customizeBoostrap(settings);
+
+        Bootstrap.Builder builder = Bootstrap.builder().withBootstrapSettings(settings);
+        customizeBootstrap(builder);
+
+        builder.install(this);
+    }
+
+    protected void customizeBoostrap(IBootstrapSettings settings) {
+    }
+
+    protected void customizeBootstrap(Bootstrap.Builder builder) {
+    }
+
+    protected void optimize(ResourceSettings resourceSettings) {
+        if (usesDeploymentConfig()) {
+            resourceSettings.setCachingStrategy(new FilenameWithVersionResourceCachingStrategy(
+                "-v-",
+                new CachingResourceVersion(new Adler32ResourceVersion())
+            ));
+
+            resourceSettings.setJavaScriptCompressor(new GoogleClosureJavaScriptCompressor(CompilationLevel.SIMPLE_OPTIMIZATIONS));
+            resourceSettings.setCssCompressor(new YuiCssCompressor());
+
+            getFrameworkSettings().setSerializer(new DeflatedJavaSerializer(getApplicationKey()));
+        } else {
+            resourceSettings.setCachingStrategy(new NoOpResourceCachingStrategy());
+        }
+
+        if (StringUtils.hasText(Bootstrap.getSettings().getJsResourceFilterName()))
+            getHeaderResponseDecorators().addPostProcessingDecorator(new RenderJavaScriptToFooterHeaderResponseDecorator());
+        getRequestCycleSettings().setRenderStrategy(RequestCycleSettings.RenderStrategy.ONE_PASS_RENDER);
+    }
+
+    private void configureDebug(DebugSettings debugSettings) {
+        if (matches(DEPLOYMENT)) {
+            debugSettings.setComponentUseCheck(false);
+        } else if (matches(DEVELOPMENT)) {
+            debugSettings.setComponentPathAttributeName("data-wicket-path");
+            debugSettings.setOutputMarkupContainerClassName(true);
+        }
+    }
+
+    protected void installSpringComponentScanning() {
+    }
+
+    private boolean matches(RuntimeConfigurationType configurationType) {
+        return Objects.equals(configurationType, getConfigurationType());
+    }
+}
--- src/main/java/de/vinado/wicket/participate/ManagementApplication.java
+++ src/main/java/de/vinado/wicket/participate/ManagementApplication.java
@@ -1,133 +1,72 @@
 package de.vinado.wicket.participate;
 
-import com.google.javascript.jscomp.CompilationLevel;
-import de.agilecoders.wicket.core.Bootstrap;
-import de.agilecoders.wicket.core.markup.html.RenderJavaScriptToFooterHeaderResponseDecorator;
-import de.agilecoders.wicket.core.request.resource.caching.version.Adler32ResourceVersion;
-import de.agilecoders.wicket.core.settings.BootstrapSettings;
 import de.agilecoders.wicket.core.settings.IBootstrapSettings;
-import de.agilecoders.wicket.extensions.javascript.GoogleClosureJavaScriptCompressor;
-import de.agilecoders.wicket.extensions.javascript.YuiCssCompressor;
+import de.vinado.wicket.bt4.AuthenticatedBootstrapWebApplication;
 import de.vinado.wicket.participate.configuration.CryptoProperties;
 import de.vinado.wicket.participate.ui.event.EventsPage;
 import de.vinado.wicket.participate.ui.login.SignInPage;
-import de.vinado.wicket.participate.ui.pages.ErrorPage;
-import de.vinado.wicket.participate.ui.pages.ExpiredPage;
+import de.vinado.wicket.participate.ui.pages.PageRegistrar;
 import de.vinado.wicket.participate.ui.pages.PageRegistry;
 import lombok.RequiredArgsConstructor;
-import org.apache.wicket.RuntimeConfigurationType;
+import lombok.Setter;
+import org.apache.wicket.Page;
 import org.apache.wicket.authentication.strategy.DefaultAuthenticationStrategy;
 import org.apache.wicket.authroles.authentication.AbstractAuthenticatedWebSession;
-import org.apache.wicket.authroles.authentication.AuthenticatedWebApplication;
-import org.apache.wicket.authroles.authorization.strategies.role.annotations.AnnotationsRoleAuthorizationStrategy;
-import org.apache.wicket.core.request.handler.PageProvider;
-import org.apache.wicket.core.request.handler.RenderPageRequestHandler;
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.protocol.https.HttpsConfig;
-import org.apache.wicket.protocol.https.HttpsMapper;
-import org.apache.wicket.request.IRequestHandler;
-import org.apache.wicket.request.cycle.IRequestCycleListener;
-import org.apache.wicket.request.cycle.RequestCycle;
-import org.apache.wicket.request.resource.caching.FilenameWithVersionResourceCachingStrategy;
-import org.apache.wicket.request.resource.caching.NoOpResourceCachingStrategy;
-import org.apache.wicket.request.resource.caching.version.CachingResourceVersion;
-import org.apache.wicket.serialize.java.DeflatedJavaSerializer;
-import org.apache.wicket.settings.RequestCycleSettings;
+import org.apache.wicket.settings.SecuritySettings;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 import org.apache.wicket.util.crypt.SunJceCrypt;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import java.nio.charset.StandardCharsets;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
+/**
+ * @author Vincent Nadoll
+ */
 @Component
 @RequiredArgsConstructor
 @EnableConfigurationProperties({CryptoProperties.class})
-public class ManagementApplication extends AuthenticatedWebApplication {
+public class ManagementApplication extends AuthenticatedBootstrapWebApplication implements ApplicationContextAware {
 
     private final CryptoProperties cryptoProperties;
 
-    @Override
-    protected void internalInit() {
-        super.internalInit();
-
-        getApplicationSettings().setPageExpiredErrorPage(ExpiredPage.class);
-        getApplicationSettings().setInternalErrorPage(ErrorPage.class);
-    }
+    @Setter
+    private ApplicationContext applicationContext;
 
     @Override
-    public void init() {
-        super.init();
+    protected void configureSecurity(SecuritySettings securitySettings) {
+        super.configureSecurity(securitySettings);
 
-        getCspSettings().blocking().disabled();
-
-        getApplicationSettings().setUploadProgressUpdatesEnabled(true);
-        getMarkupSettings().setDefaultMarkupEncoding("UTF-8");
-        getSecuritySettings().setAuthorizationStrategy(new AnnotationsRoleAuthorizationStrategy(
-            roles -> ParticipateSession.get().getRoles().hasAnyRole(roles)));
-        SunJceCrypt crypt = new SunJceCrypt(cryptoProperties.getPbeSalt().getBytes(StandardCharsets.UTF_8), cryptoProperties.getPbeIterationCount());
+        SunJceCrypt crypt = new SunJceCrypt(cryptoProperties.getPbeSalt().getBytes(StandardCharsets.UTF_8),
+            cryptoProperties.getPbeIterationCount());
         crypt.setKey(cryptoProperties.getSessionSecret());
         DefaultAuthenticationStrategy authenticationStrategy = new DefaultAuthenticationStrategy("_login", crypt);
-        getSecuritySettings().setAuthenticationStrategy(authenticationStrategy);
-        setRootRequestMapper(new HttpsMapper(getRootRequestMapper(), new HttpsConfig(80, 443)));
-        getMarkupSettings().setStripWicketTags(true);
-
-        PageRegistry.getInstance()
-            .forEach(registration -> mountPage(registration.getPath(), registration.getPageClass()));
-
-        // Install Bootstrap
-        configureBootstrap();
-        optimizeForWebPerformance();
-
-        // Deployment vs. Development Mode
-        if (RuntimeConfigurationType.DEPLOYMENT.equals(getConfigurationType())) {
-            getResourceSettings().setResourcePollFrequency(null);
-            getDebugSettings().setComponentUseCheck(false);
-            getMarkupSettings().setCompressWhitespace(true);
-            getMarkupSettings().setStripComments(true);
-        } else if (RuntimeConfigurationType.DEVELOPMENT.equals(getConfigurationType())) {
-            getDebugSettings().setComponentPathAttributeName("data-wicket-path");
-            getDebugSettings().setOutputMarkupContainerClassName(true);
-        }
-
-        // spring injector for @SpringBean annotations
-        WebApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(getServletContext());
-        getComponentInstantiationListeners().add(new SpringComponentInjector(this, ctx));
-
-        // Error Page
-        getRequestCycleListeners().add(new IRequestCycleListener() {
-            @Override
-            public IRequestHandler onException(final RequestCycle cycle, final Exception ex) {
-                return new RenderPageRequestHandler(new PageProvider(new ErrorPage(ex)));
-            }
-        });
+        securitySettings.setAuthenticationStrategy(authenticationStrategy);
     }
 
-    private void configureBootstrap() {
-        final IBootstrapSettings settings = new BootstrapSettings();
+    @Override
+    protected void mountPages() {
+        PageRegistry.getInstance().forEach(unwrapped(this::mountPage));
+    }
+
+    private static Consumer<PageRegistrar> unwrapped(BiConsumer<String, Class<? extends Page>> action) {
+        return registrar -> action.accept(registrar.getPath(), registrar.getPageClass());
+    }
+
+    @Override
+    protected void installSpringComponentScanning() {
+        getComponentInstantiationListeners().add(new SpringComponentInjector(this, applicationContext));
+    }
+
+    @Override
+    protected void customizeBoostrap(IBootstrapSettings settings) {
+        super.customizeBoostrap(settings);
         settings.setJsResourceFilterName("footer-container");
-        Bootstrap.builder().withBootstrapSettings(settings).install(this);
-    }
-
-    private void optimizeForWebPerformance() {
-        if (usesDeploymentConfig()) {
-            getResourceSettings().setCachingStrategy(new FilenameWithVersionResourceCachingStrategy(
-                "-v-",
-                new CachingResourceVersion(new Adler32ResourceVersion())
-            ));
-
-            getResourceSettings().setJavaScriptCompressor(new GoogleClosureJavaScriptCompressor(CompilationLevel.SIMPLE_OPTIMIZATIONS));
-            getResourceSettings().setCssCompressor(new YuiCssCompressor());
-
-            getFrameworkSettings().setSerializer(new DeflatedJavaSerializer(getApplicationKey()));
-        } else {
-            getResourceSettings().setCachingStrategy(new NoOpResourceCachingStrategy());
-        }
-
-        setHeaderResponseDecorator(new RenderJavaScriptToFooterHeaderResponseDecorator());
-        getRequestCycleSettings().setRenderStrategy(RequestCycleSettings.RenderStrategy.ONE_PASS_RENDER);
     }
 
     @Override
