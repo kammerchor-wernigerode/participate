c2fab2d72f1decae324d87a21ecf925adf66d716
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-03-25 10:08:51 +0100

    Cleanup code

--- src/main/java/de/vinado/wicket/participate/configuration/WebInitializer.java
+++ src/main/java/de/vinado/wicket/participate/configuration/WebInitializer.java
@@ -1,54 +1,41 @@
 package de.vinado.wicket.participate.configuration;
 
 import de.vinado.wicket.participate.ui.pages.ManagementPageRegistry;
+import lombok.RequiredArgsConstructor;
 import org.apache.wicket.protocol.http.WicketFilter;
 import org.apache.wicket.spring.SpringWebApplicationFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.ContextCleanupListener;
 
+import java.util.EnumSet;
 import java.util.function.Function;
+import java.util.stream.Stream;
 import javax.servlet.DispatcherType;
 import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import java.util.EnumSet;
-import java.util.stream.Stream;
 
 /**
  * Wicket request handling configuration for Servlet 3.0+ and replacement for web.xml.
  */
 @Configuration
+@RequiredArgsConstructor
 public class WebInitializer implements ServletContextInitializer {
 
     static final String APP_ROOT = "/_";
 
     private final ApplicationProperties properties;
 
-    @Autowired
-    public WebInitializer(final ApplicationProperties properties) {
-        this.properties = properties;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
     @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-        final FilterRegistration filter = servletContext.addFilter("wicket.participate.management", WicketFilter.class);
+    public void onStartup(ServletContext servletContext) {
+        FilterRegistration filter = servletContext.addFilter("wicket.participate.management", WicketFilter.class);
         filter.setInitParameter(WicketFilter.APP_FACT_PARAM, SpringWebApplicationFactory.class.getName());
         filter.setInitParameter("applicationClassName", "de.vinado.wicket.participate.ManagementApplication");
         filter.setInitParameter("applicationBean", "managementApplication");
         filter.setInitParameter(WicketFilter.IGNORE_PATHS_PARAM, "/static");
         filter.setInitParameter(WicketFilter.FILTER_MAPPING_PARAM, APP_ROOT + "/*");
         filter.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST, DispatcherType.ERROR), false, getUrlPatterns());
-        if (properties.isDevelopmentMode()) {
-            filter.setInitParameter("configuration", "development");
-        } else {
-            filter.setInitParameter("configuration", "deployment");
-        }
-
+        filter.setInitParameter("configuration", properties.isDevelopmentMode() ? "development" : "deployment");
         servletContext.addListener(new ContextCleanupListener());
     }
 
