1e62c750971955106dc58a21820115827358c990
Vincent Nadoll <vincent.nadoll@googlemail.com>
2022-03-29 01:18:49 +0200

    Remove intermediate panel

--- src/main/java/de/vinado/wicket/participate/ui/event/EventsPage.java
+++ src/main/java/de/vinado/wicket/participate/ui/event/EventsPage.java
@@ -1,33 +1,106 @@
 package de.vinado.wicket.participate.ui.event;
 
-import de.agilecoders.wicket.core.markup.html.bootstrap.navigation.Breadcrumb;
+import de.vinado.wicket.common.UpdateOnEventBehavior;
+import de.vinado.wicket.participate.ParticipateSession;
+import de.vinado.wicket.participate.components.PersonContext;
+import de.vinado.wicket.participate.events.RemoveEventUpdateEvent;
+import de.vinado.wicket.participate.model.EventDetails;
+import de.vinado.wicket.participate.model.filters.EventFilter;
+import de.vinado.wicket.participate.model.filters.ParticipantFilter;
+import de.vinado.wicket.participate.services.EventService;
 import de.vinado.wicket.participate.ui.pages.ParticipatePage;
-import org.apache.wicket.extensions.breadcrumb.panel.BreadCrumbPanel;
+import org.apache.wicket.IGenericComponent;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.event.IEvent;
+import org.apache.wicket.model.CompoundPropertyModel;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.spring.injection.annot.SpringBean;
 
 /**
- * Base page for the application.
+ * @author Vincent Nadoll
  */
-public class EventsPage extends ParticipatePage {
+public class EventsPage extends ParticipatePage implements IGenericComponent<EventDetails, EventsPage> {
 
-    public EventsPage() {
-        this(new PageParameters());
-    }
+    private static final long serialVersionUID = -4123194846716920977L;
 
-    /**
-     * @param parameters {@link org.apache.wicket.request.mapper.parameter.PageParameters}
-     */
-    public EventsPage(final PageParameters parameters) {
+    @SpringBean
+    private EventService eventService;
+
+    @SpringBean
+    private PersonContext personContext;
+
+    private EventsPanel eventListPanel;
+    private EventPanel eventPanel;
+
+    public EventsPage(PageParameters parameters) {
         super(parameters);
 
-        final Breadcrumb breadcrumb = new Breadcrumb("breadcrumb");
-        breadcrumb.setOutputMarkupPlaceholderTag(true);
-        breadcrumb.setVisible(false);
-        add(breadcrumb);
+        final EventDetails eventView;
+        if (null == ParticipateSession.get().getEvent()) {
+            if (eventService.hasUpcomingEvents()) {
+                eventView = eventService.getLatestEventDetails();
+            } else {
+                eventView = new EventDetails();
+            }
+        } else {
+            eventView = eventService.getEventDetails(ParticipateSession.get().getEvent());
+        }
 
-        final BreadCrumbPanel breadCrumbPanel = new EventMasterPanel("eventPanel", breadcrumb);
-        breadCrumbPanel.setOutputMarkupId(true);
-        breadcrumb.setActive(breadCrumbPanel);
-        add(breadCrumbPanel);
+        setModel(new CompoundPropertyModel<>(eventView));
+    }
+
+    private EventDetails getLatestEventDetails() {
+        EventDetails event = eventService.getLatestEventDetails();
+        return null == event ? new EventDetails() : event;
+    }
+
+    @Override
+    protected void onInitialize() {
+        super.onInitialize();
+
+        IModel<EventFilter> filterModel = new CompoundPropertyModel<>(filterModel());
+        eventListPanel = new EventsPanel("events", filterModel);
+        eventListPanel.add(new UpdateOnEventBehavior<>(EventTableUpdateIntent.class));
+        eventListPanel.add(new UpdateOnEventBehavior<>(EventFilterIntent.class));
+        eventListPanel.setOutputMarkupId(true);
+        add(eventListPanel);
+
+        IModel<ParticipantFilter> participantFilter = new CompoundPropertyModel<>(new ParticipantFilter());
+        eventPanel = new EventPanel("event", null, getModel(), true, personContext, participantFilter) {
+            @Override
+            protected void onConfigure() {
+                super.onConfigure();
+                setVisible(eventService.hasUpcomingEvents());
+            }
+        };
+        eventPanel.setOutputMarkupPlaceholderTag(true);
+        add(eventPanel);
+    }
+
+    private EventFilter filterModel() {
+        EventFilter existing = ParticipateSession.get().getEventFilter();
+        return null == existing ? new EventFilter() : existing;
+    }
+
+    @Override
+    public void onEvent(IEvent<?> iEvent) {
+        super.onEvent(iEvent);
+        Object payload = iEvent.getPayload();
+
+        if (payload instanceof RemoveEventUpdateEvent) {
+            RemoveEventUpdateEvent event = (RemoveEventUpdateEvent) payload;
+            AjaxRequestTarget target = event.getTarget();
+
+            if (!eventService.hasUpcomingEvents()) {
+                ParticipateSession.get().setEvent(null);
+            } else {
+                ParticipateSession.get().setEvent(eventService.getLatestEvent());
+                setModel(new CompoundPropertyModel<>(getLatestEventDetails()));
+            }
+
+            target.add(eventListPanel);
+            target.add(eventPanel);
+        }
     }
 }
