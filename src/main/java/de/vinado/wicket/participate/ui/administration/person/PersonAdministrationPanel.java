dc4471b1416b3e924476f7c4984bf6e811c2f71a
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-02-25 17:27:12 +0100

    Add empty person table

--- src/main/java/de/vinado/wicket/participate/ui/administration/person/PersonAdministrationPanel.java
+++ src/main/java/de/vinado/wicket/participate/ui/administration/person/PersonAdministrationPanel.java
@@ -1,14 +1,24 @@
 package de.vinado.wicket.participate.ui.administration.person;
 
 import de.vinado.wicket.participate.model.Person;
+import de.vinado.wicket.participate.person.model.PersonRepository;
+import de.vinado.wicket.repeater.table.FunctionalDataProvider;
 import lombok.RequiredArgsConstructor;
 import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.markup.html.panel.EmptyPanel;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public class PersonAdministrationPanel extends Panel {
 
     private static final long serialVersionUID = 2120435783576012933L;
+
+    @SpringBean
+    private PersonRepository personRepository;
 
     public PersonAdministrationPanel(String id) {
         super(id);
@@ -22,6 +32,46 @@ public class PersonAdministrationPanel extends Panel {
     }
 
     private WebMarkupContainer deletedPersonTable(String id) {
-        return new EmptyPanel(id);
+        PersonDataProvider dataProvider = new PersonDataProvider(personRepository);
+        return new PersonAdministrationTable(id, dataProvider);
+    }
+
+
+    @RequiredArgsConstructor
+    private static class PersonDataProvider extends FunctionalDataProvider<Person> {
+
+        private static final long serialVersionUID = 8241723874342037449L;
+
+        private final PersonRepository repository;
+
+        @Override
+        protected Stream<Person> load() {
+            return repository.listInactivePersons();
+        }
+
+        @Override
+        public IModel<Person> model(Person person) {
+            return new PersonModel(person);
+        }
+
+        private static class PersonModel extends Model<Person> {
+
+            private static final long serialVersionUID = -2205664904469033653L;
+
+            public PersonModel(Person person) {
+                super(person);
+            }
+
+            @Override
+            public int hashCode() {
+                Person person = getObject();
+                return Objects.hash(
+                    person.getId(),
+                    person.getFirstName(),
+                    person.getLastName(),
+                    person.getEmail()
+                );
+            }
+        }
     }
 }
---
+++ src/main/java/de/vinado/wicket/participate/ui/administration/person/PersonAdministrationTable.java
@@ -0,0 +1,32 @@
+package de.vinado.wicket.participate.ui.administration.person;
+
+import de.vinado.wicket.participate.components.tables.BootstrapAjaxDataTable;
+import de.vinado.wicket.participate.model.Person;
+import de.vinado.wicket.repeater.table.FunctionalDataProvider;
+import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder;
+import org.apache.wicket.extensions.markup.html.repeater.data.table.IColumn;
+import org.apache.wicket.markup.repeater.ReuseIfModelsEqualStrategy;
+import org.danekja.java.util.function.serializable.SerializableFunction;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonAdministrationTable extends BootstrapAjaxDataTable<Person, SerializableFunction<Person, ?>> {
+
+    private static final long serialVersionUID = -8173386174805574030L;
+
+    public PersonAdministrationTable(String id, FunctionalDataProvider<Person> dataProvider) {
+        super(id, columns(), dataProvider, Integer.MAX_VALUE);
+
+        dataProvider.setSort(Person::getSortName, SortOrder.ASCENDING);
+        setOutputMarkupId(true);
+        condensed().hover();
+
+        setItemReuseStrategy(ReuseIfModelsEqualStrategy.getInstance());
+    }
+
+    private static List<? extends IColumn<Person, SerializableFunction<Person, ?>>> columns() {
+        ArrayList<IColumn<Person, SerializableFunction<Person, ?>>> columns = new ArrayList<>();
+        return columns;
+    }
+}
