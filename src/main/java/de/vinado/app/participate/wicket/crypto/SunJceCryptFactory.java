22612cd298f87a80cf14dd5e91e9b3ffc8421a40
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-04-10 14:47:08 +0200

    Bind strong crypt exclusively to deployment mode

--- src/main/java/de/vinado/app/participate/wicket/crypto/SunJceCryptFactory.java
+++ src/main/java/de/vinado/app/participate/wicket/crypto/SunJceCryptFactory.java
@@ -3,12 +3,14 @@ package de.vinado.app.participate.wicket.crypto;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.apache.wicket.util.crypt.SunJceCrypt;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @EnableConfigurationProperties(CryptoProperties.class)
+@ConditionalOnProperty(prefix = "app.wicket", name = "runtime-configuration", havingValue = "DEPLOYMENT", matchIfMissing = true)
 public class SunJceCryptFactory implements CryptFactory {
 
     @NonNull
--- src/main/java/de/vinado/app/participate/wicket/crypto/TrivialCryptFactory.java
+++ src/main/java/de/vinado/app/participate/wicket/crypto/TrivialCryptFactory.java
@@ -2,7 +2,11 @@ package de.vinado.app.participate.wicket.crypto;
 
 import org.apache.wicket.util.crypt.ICrypt;
 import org.apache.wicket.util.crypt.TrivialCrypt;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
 
+@Component
+@ConditionalOnProperty(prefix = "app.wicket", name = "runtime-configuration", havingValue = "DEVELOPMENT")
 public class TrivialCryptFactory implements CryptFactory {
 
     @Override
