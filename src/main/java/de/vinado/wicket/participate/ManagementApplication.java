755763287bc4171b3cbddaa1a97b3c914c1a1837
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-04-10 14:17:13 +0200

    Remove unused imports

--- src/main/java/de/vinado/wicket/participate/ManagementApplication.java
+++ src/main/java/de/vinado/wicket/participate/ManagementApplication.java
@@ -18,8 +18,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
-import java.nio.charset.StandardCharsets;
-
 /**
  * @author Vincent Nadoll
  */
--- src/main/java/de/vinado/wicket/participate/wicket/form/app/FormApplication.java
+++ src/main/java/de/vinado/wicket/participate/wicket/form/app/FormApplication.java
@@ -31,7 +31,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Supplier;
