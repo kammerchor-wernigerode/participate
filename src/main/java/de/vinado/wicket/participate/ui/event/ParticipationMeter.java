69c14c2fcb8c044af59382c6eae0ebf786494dba
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-05-10 14:03:54 +0200

    Inline grid-template-columns value

--- src/main/java/de/vinado/wicket/participate/ui/event/ParticipationMeter.java
+++ src/main/java/de/vinado/wicket/participate/ui/event/ParticipationMeter.java
@@ -1,12 +1,21 @@
 package de.vinado.wicket.participate.ui.event;
 
 import de.vinado.wicket.participate.model.EventDetails;
+import lombok.RequiredArgsConstructor;
 import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
 import org.apache.wicket.IGenericComponent;
+import org.apache.wicket.StyleAttributeModifier;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.GenericPanel;
 import org.apache.wicket.model.IModel;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ParticipationMeter extends GenericPanel<EventDetails> {
 
@@ -21,6 +30,8 @@ public class ParticipationMeter extends GenericPanel<EventDetails> {
         add(accepted("a"));
         add(declined("d"));
         add(pending("p"));
+
+        add(new GridTemplateColumns(getModel()));
     }
 
     private Component accepted(String wicketId) {
@@ -59,6 +70,47 @@ public class ParticipationMeter extends GenericPanel<EventDetails> {
             super.onInitialize();
 
             add(AttributeModifier.append("class", getId()));
+        }
+    }
+
+    @RequiredArgsConstructor
+    private static class GridTemplateColumns extends StyleAttributeModifier {
+
+        private final IModel<EventDetails> model;
+
+        @Override
+        protected Map<String, String> update(Map<String, String> oldStyles) {
+            Map<String, String> styles = new HashMap<>(oldStyles);
+            styles.put("grid-template-columns", value());
+            return styles;
+        }
+
+        private String value() {
+            return fractions()
+                .map(append("fr"))
+                .collect(Collectors.joining(" "));
+        }
+
+        private Stream<Long> fractions() {
+            long accepted = get(EventDetails::getAcceptedSum);
+            long declined = get(EventDetails::getDeclinedCount);
+            long pending = get(EventDetails::getPendingCount);
+            return Stream.of(accepted, declined, pending);
+        }
+
+        private long get(Function<EventDetails, Long> extractor) {
+            return event()
+                .map(extractor)
+                .orElse(1L);
+        }
+
+        private Optional<EventDetails> event() {
+            return Optional.ofNullable(model)
+                .map(IModel::getObject);
+        }
+
+        private static <T> Function<? super T, String> append(String suffix) {
+            return self -> self + suffix;
         }
     }
 }
