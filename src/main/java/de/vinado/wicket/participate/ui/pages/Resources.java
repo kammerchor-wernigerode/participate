b849f1b8bd5cd3a2ce64a7c06e22b433b74ca686
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-10-20 13:59:29 +0200

    Simplify favicon diversity

--- src/main/java/de/vinado/wicket/participate/ui/pages/Resources.java
+++ src/main/java/de/vinado/wicket/participate/ui/pages/Resources.java
@@ -14,22 +14,10 @@ import org.apache.wicket.markup.head.JavaScriptReferenceHeaderItem;
 import org.apache.wicket.markup.head.MetaDataHeaderItem;
 import org.apache.wicket.markup.head.filter.FilteredHeaderItem;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
-
 import static org.apache.wicket.markup.head.HtmlImportHeaderItem.forLinkTag;
-import static org.apache.wicket.markup.head.MetaDataHeaderItem.forMetaTag;
 
 @UtilityClass
 public final class Resources {
-
-    private static final int[] appleTouchIconSizes;
-    private static final int[] iconSizes;
-
-    static {
-        appleTouchIconSizes = new int[]{57, 60, 72, 76, 114, 120, 144, 152, 180};
-        iconSizes = new int[]{96, 32, 16};
-    }
 
     public static void render(IHeaderResponse response, Component host) {
         response.render(MetaDataHeaderItem.forMetaTag("viewport", "width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no"));
@@ -45,41 +33,7 @@ public final class Resources {
     }
 
     public static void renderFavicons(IHeaderResponse response) {
-        appleTouchIcons().forEach(response::render);
-        response.render(androidIcon());
-        pngIcons().forEach(response::render);
-        response.render(forLinkTag("manifest", "/manifest.json"));
-        response.render(forMetaTag("msapplication-TileColor", "#ffffff"));
-        response.render(forMetaTag("msapplication-TileImage", "/ms-icon-144x144.png"));
-        response.render(forMetaTag("theme-color", "#ffffff"));
-    }
-
-    private static Stream<MetaDataHeaderItem> appleTouchIcons() {
-        return Arrays.stream(appleTouchIconSizes).mapToObj(Resources::appleTouchIcon);
-    }
-
-    private static MetaDataHeaderItem appleTouchIcon(int size) {
-        String dimension = size + "x" + size;
-        String path = "/apple-icon-" + dimension + ".png";
-        return forLinkTag("apple-touch-icon", path)
-            .addTagAttribute("sizes", dimension);
-    }
-
-    private static MetaDataHeaderItem androidIcon() {
-        return forLinkTag("icon", "/android-icon-192x192.png")
-            .addTagAttribute("type", "image/png")
-            .addTagAttribute("sizes", "192x192");
-    }
-
-    private static Stream<MetaDataHeaderItem> pngIcons() {
-        return Arrays.stream(iconSizes).mapToObj(Resources::pngIcon);
-    }
-
-    private static MetaDataHeaderItem pngIcon(int size) {
-        String dimension = size + "x" + size;
-        String href = "/favicon-" + dimension + ".png";
-        return forLinkTag("icon", href)
-            .addTagAttribute("type", "image/png")
-            .addTagAttribute("sizes", dimension);
+        response.render(forLinkTag("icon", "/favicon.svg").addTagAttribute("type", "image/svg+xml"));
+        response.render(forLinkTag("icon", "/favicon.png").addTagAttribute("type", "image/png"));
     }
 }
