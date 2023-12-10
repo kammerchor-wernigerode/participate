f4e94fcab3975c48b7c3bf059132f039cf896d55
Vincent Nadoll <vincent.nadoll@googlemail.com>
2023-12-10 14:31:39 +0100

    Make form component label updatable

--- src/main/java/de/vinado/app/participate/wicket/form/FormComponentLabel.java
+++ src/main/java/de/vinado/app/participate/wicket/form/FormComponentLabel.java
@@ -3,9 +3,9 @@ package de.vinado.app.participate.wicket.form;
 import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.MarkupStream;
 import org.apache.wicket.markup.html.form.FormComponent;
-import org.apache.wicket.markup.html.form.SimpleFormComponentLabel;
+import org.apache.wicket.model.IModel;
 
-public class FormComponentLabel extends SimpleFormComponentLabel {
+public class FormComponentLabel extends org.apache.wicket.markup.html.form.FormComponentLabel {
 
     private final FormComponent<?> formComponent;
 
@@ -13,6 +13,20 @@ public class FormComponentLabel extends SimpleFormComponentLabel {
         super(id, formComponent);
 
         this.formComponent = formComponent;
+    }
+
+    @Override
+    protected void onConfigure() {
+        super.onConfigure();
+
+        IModel<String> label = formComponent.getLabel();
+        if (null == label) {
+            throw new IllegalStateException("Provided form component does not have a label set. "
+                + "Use FormComponent.setLabel(IModel) to set the model "
+                + "that will feed this label");
+        }
+
+        setDefaultModel(label);
     }
 
     @Override
