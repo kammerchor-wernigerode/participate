30c01591217e25ba07977c4ad1e0926bd20064c5
Vincent Nadoll <vincent.nadoll@googlemail.com>
2024-04-03 22:50:55 +0200

    Adapt env var version of properties

--- src/main/java/de/vinado/wicket/participate/configuration/factories/DockerSecretProcessor.java
+++ src/main/java/de/vinado/wicket/participate/configuration/factories/DockerSecretProcessor.java
@@ -17,12 +17,12 @@ public class DockerSecretProcessor extends SecretsEnvironmentPostProcessor {
     @Override
     protected PropertyIndexSupplier getPropertyIndexSupplier(ConfigurableEnvironment environment) {
         Map<String, String> properties = new HashMap<>();
-        properties.put("spring.datasource.username", "DATABASE_USER_FILE");
-        properties.put("spring.datasource.password", "DATABASE_PASSWORD_FILE");
-        properties.put("spring.mail.username", "SMTP_USER_FILE");
-        properties.put("spring.mail.password", "SMTP_PASSWORD_FILE");
-        properties.put("app.crypto.session-secret", "CRYPTO_SESSION_SECRET_FILE");
-        properties.put("app.crypto.pbe-salt", "CRYPTO_PBE_SALT_FILE");
+        properties.put("spring.datasource.username", "SPRING_DATASOURCE_USERNAME_FILE");
+        properties.put("spring.datasource.password", "SPRING_DATASOURCE_PASSWORD_FILE");
+        properties.put("spring.mail.username", "SPRING_MAIL_USERNAME_FILE");
+        properties.put("spring.mail.password", "SPRING_MAIL_PASSWORD_FILE");
+        properties.put("app.crypto.session-secret", "APP_CRYPTO_SESSION_SECRET_FILE");
+        properties.put("app.crypto.pbe-salt", "APP_CRYPTO_PBE_SALT_FILE");
         properties.put("spring.security.oauth2.client.registration.keycloak.client-secret", "SPRING_SECURITY_OAUTH2_CLIENT_REGISTRATION_KEYCLOAK_CLIENT_SECRET_FILE");
 
         return PropertyIndexSupplier.from(properties)
