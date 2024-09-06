6a22b454db25b22b6e22e59962281f0396872391
Vincent Nadoll <vincent.nadoll@googlemail.com>
2024-09-06 16:08:23 +0200

    Add email dispatcher based on Spring's sender

---
+++ src/main/java/de/vinado/wicket/participate/notification/email/infrastructure/JavaMailEmailDispatcher.java
@@ -0,0 +1,59 @@
+package de.vinado.wicket.participate.notification.email.infrastructure;
+
+import de.vinado.wicket.participate.notification.email.model.Email;
+import de.vinado.wicket.participate.notification.email.model.EmailDispatcher;
+import de.vinado.wicket.participate.notification.email.model.EmailException;
+import de.vinado.wicket.participate.notification.email.model.Recipient;
+import de.vinado.wicket.participate.notification.email.model.Transmission;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class JavaMailEmailDispatcher implements EmailDispatcher {
+
+    private final JavaMailSender sender;
+
+    @Override
+    public void dispatch(Email email, Transmission... transmissions) throws EmailException {
+        try {
+            for (Transmission transmission : transmissions) {
+                dispatch(email, transmission);
+            }
+        } catch (Exception e) {
+            throw new EmailException(e);
+        }
+    }
+
+    public void dispatch(Email email, Transmission transmission) throws MessagingException {
+        MimeMessage message = sender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, StandardCharsets.UTF_8.name());
+
+        helper.setSubject(email.subject());
+        helper.setFrom(transmission.sender().address());
+
+        for (Recipient recipient : transmission.recipients()) {
+            message.addRecipients(recipient.type(), new InternetAddress[]{recipient.address()});
+        }
+
+        Optional<String> text = email.textContent();
+        if (text.isPresent()) {
+            helper.setText(text.get(), false);
+        }
+
+        Optional<String> html = email.htmlContent();
+        if (html.isPresent()) {
+            helper.setText(html.get(), true);
+        }
+
+        sender.send(message);
+    }
+}
