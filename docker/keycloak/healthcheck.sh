0eec9dd4537d20d36c538ecdabb5e23423c02013
Vincent Nadoll <vincent.nadoll@googlemail.com>
2025-10-20 17:52:07 +0200

    Update local Keycloak service to version 26.4

--- docker/keycloak/healthcheck.sh
+++ docker/keycloak/healthcheck.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
+set -euo pipefail
 
-exec 3<>/dev/tcp/localhost/${KC_HTTP_PORT:-8080}
+exec 3<>/dev/tcp/localhost/${KC_HTTP_MANAGEMENT_PORT:-9000}
 
-echo -e "GET /health/ready HTTP/1.1\nhost: localhost:${KC_HTTP_PORT:-8080}\n" >&3
+{
+    printf "GET %s/health/ready HTTP/1.1\r\n" "${KC_HTTP_MANAGEMENT_RELATIVE_PATH:-${KC_HTTP_RELATIVE_PATH:-}}";
+    printf "Host: localhost:%s\r\n" "${KC_HTTP_MANAGEMENT_PORT:-9000}";
+    printf "User-Agent: healthcheck\r\n";
+    printf "Connection: close\r\n";
+    printf "\r\n";
+} >&3
 
-timeout --preserve-status 1 cat <&3 | grep -m 1 status | grep -m 1 UP
+timeout --preserve-status 2 cat <&3 | grep -m1 -E '"status"[[:space:]]*:[[:space:]]*"UP"'
 error=$?
 
-exec 3<&-
-exec 3>&-
+exec 3<&- 3>&-
 
 exit $error
