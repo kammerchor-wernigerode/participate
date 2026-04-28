dab789edf0dc97549f2db094a2a9be00b705adf9
Vincent Nadoll <vincent.nadoll@googlemail.com>
2026-04-28 13:21:08 +0200

    Improve column filter layout

--- src/main/java/de/kammerchorwernigerode/app/participate/event/presentation/components/InvitationStatusFilter.java
+++ src/main/java/de/kammerchorwernigerode/app/participate/event/presentation/components/InvitationStatusFilter.java
@@ -2,11 +2,17 @@ package de.kammerchorwernigerode.app.participate.event.presentation.components;
 
 import de.kammerchorwernigerode.app.participate.event.presentation.model.InvitationStatusSelection;
 import de.kammerchorwernigerode.app.participate.wicket.markup.html.bootstrap.components.TooltipBehavior;
+import org.apache.wicket.Component;
 import org.apache.wicket.IGenericComponent;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.form.AjaxFormComponentUpdatingBehavior;
+import org.apache.wicket.ajax.markup.html.form.AjaxSubmitLink;
+import org.apache.wicket.behavior.Behavior;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.filter.AbstractFilter;
 import org.apache.wicket.extensions.markup.html.repeater.data.table.filter.FilterForm;
+import org.apache.wicket.markup.head.CssContentHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.html.TransparentWebMarkupContainer;
 import org.apache.wicket.markup.html.form.CheckBox;
 import org.apache.wicket.markup.html.form.FormComponentLabel;
 import org.apache.wicket.model.IModel;
@@ -46,6 +52,10 @@ public class InvitationStatusFilter extends AbstractFilter
         add(tentative, tentativeLabel);
 
 
+        TransparentWebMarkupContainer btnGroup1 = new TransparentWebMarkupContainer("btnGroup1");
+        btnGroup1.add(new SqueezeBehavior());
+        add(btnGroup1);
+
         IModel<Boolean> acceptedModel =
             LambdaModel.of(model, InvitationStatusSelection::isAccepted, InvitationStatusSelection::setAccepted);
         CheckBox accepted = new CheckBox("accepted", acceptedModel);
@@ -62,12 +72,33 @@ public class InvitationStatusFilter extends AbstractFilter
         add(declined, declinedLabel);
 
 
+        TransparentWebMarkupContainer btnGroup2 = new TransparentWebMarkupContainer("btnGroup2");
+        btnGroup2.add(new SqueezeBehavior());
+        add(btnGroup2);
+
         IModel<Boolean> pendingModel =
             LambdaModel.of(model, InvitationStatusSelection::isPending, InvitationStatusSelection::setPending);
         CheckBox pending = new CheckBox("pending", pendingModel);
         FormComponentLabel pendingLabel = new FormComponentLabel("pendingLabel", pending);
         pendingLabel.add(new TooltipBehavior(new ResourceModel("InvitationStatus.PENDING")));
         add(pending, pendingLabel);
+
+        AjaxSubmitLink reset = new AjaxSubmitLink("reset", form) {
+
+            @Override
+            protected void onSubmit(AjaxRequestTarget target) {
+                InvitationStatusSelection selection = model.getObject();
+                selection.setUninvited(false);
+                selection.setTentative(false);
+                selection.setAccepted(false);
+                selection.setDeclined(false);
+                selection.setPending(false);
+
+                target.add(form);
+            }
+        };
+        add(reset);
+
 
         form.visitChildren(CheckBox.class, (component, visit) -> {
             if (component instanceof CheckBox checkbox) {
@@ -84,5 +115,24 @@ public class InvitationStatusFilter extends AbstractFilter
                 });
             }
         });
+    }
+
+
+    private static class SqueezeBehavior extends Behavior {
+
+        @Override
+        public void bind(Component component) {
+            component.setOutputMarkupId(true);
+        }
+
+        @Override
+        public void renderHead(Component component, IHeaderResponse response) {
+            String markupId = component.getMarkupId();
+            response.render(CssContentHeaderItem.forCSS("""
+                #%s {
+                    margin-top: -1px;
+                }\
+                """.formatted(markupId), markupId + "-style"));
+        }
     }
 }
